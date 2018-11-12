a64aeabdf862e077744444b9b62cb0c1241ff178
AlexisLeConte <alexis.leconte@outlook.fr>
2018-11-12 09:49:22 +0100

    Updated arbor.js renderer Nodes are now displayed as Circles Nodes names are now displayed Edges types are now displayed

--- lab/arbor-js-test/driver.js
+++ lab/arbor-js-test/driver.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
-  var sys = arbor.ParticleSystem(1000, 600, 0.5)
+  var sys = arbor.ParticleSystem(200, 800, 0.5)
   sys.parameters({gravity:true})
   sys.renderer = renderer("#viewport")
   
-  sys.addNode('node0', {name: 'Node0', radius: 80})
-  sys.addNode('node1', {name: 'GitHub/AlexisLeConte', radius: 32, uri:'https://github.com/AlexisLeConte/IF-4-WS-Project'})
-  sys.addNode('node2', {name: 'Zimbra INSA Lyon', radius: 42, uri:'https://login.insa-lyon.fr/zimbra/'})
-  sys.addNode('node3', {name: 'Planète INSA Lyon', radius: 64, uri:'https://planete.insa-lyon.fr/uPortal/f/welcome/normal/render.uP'})
+  sys.addNode('node0', {name: 'Film', radius: 0, color: '#4444ff'})
+  sys.addNode('node1', {name: 'Director', uri:'https://github.com/AlexisLeConte/IF-4-WS-Project', radius: 0, color: '#44ff44'})
+  sys.addNode('node2', {name: 'Studio', uri:'https://login.insa-lyon.fr/zimbra/', radius: 0, color: '#ff4444'})
+  sys.addNode('node3', {name: 'Actor', uri:'https://planete.insa-lyon.fr/uPortal/f/welcome/normal/render.uP', radius: 0, color: '#44cc44'})
   sys.addEdge('node0','node1')
   sys.addEdge('node0','node2')
   sys.addEdge('node0','node3')
+  
+  setTimeout(function(){
+    var postLoadData = {
+      nodes:{
+        node4:{name: 'Node4', radius: 20, color:'blue', uri: ''},
+        node5:{name: 'Node5', radius: 30, color:'red', uri: ''},
+      },
+      edges:{
+        node0:{node4:{type:'directed by'}, node5:{type:'produced by'}},
+        node1:{node5:{type:'starred in'}},
+      }
+    };
+    sys.graft(postLoadData);
+  }, 4000);
 })
 
 function queryResourceByUri(uri) {
--- lab/arbor-js-test/renderer.js
+++ lab/arbor-js-test/renderer.js
@@ -20,27 +20,50 @@ function renderer(canvasId) {
     },
     
     redraw:function() {
-      context.fillStyle = "white"
+      context.fillStyle = 'white'
       context.fillRect(0, 0, canvas.width, canvas.height)
       
-      // edge: {source:Node, target:Node, length:#, data:{}}
+      // edge: {source:Node, target:Node, length:#, data:{type:""}}
       // pt1:  {x:#, y:#}  source position in screen coords
       // pt2:  {x:#, y:#}  target position in screen coords
       particleSystem.eachEdge(function(edge, pt1, pt2) {
-        context.strokeStyle = "#990000"
+        // metrics
+        var text = edge.data.type
+        context.font = 'bold 14pt Calibri';
+        context.textAlign = 'center';
+        var textMetrics = context.measureText(text);
+        
+        // drawing
+        context.strokeStyle = 'black'
         context.lineWidth = 2
         context.beginPath()
         context.moveTo(pt1.x, pt1.y)
         context.lineTo(pt2.x, pt2.y)
         context.stroke()
+        context.fillStyle = 'black';
+        context.fillText(text, (pt1.x + pt2.x) / 2, (pt1.y + pt2.y) / 2 + 6)
       })
 
-      // node: {mass:#, p:{x,y}, name:"", data:{}}
+      // node: {mass:#, p:{x,y}, name:"", data:{name:"", uri:"", radius:#, color:#}}
       // pt:   {x:#, y:#}  node position in screen coords
       particleSystem.eachNode(function(node, pt) {
-        var w = node.data.radius
-        context.fillStyle = "#000011"
-        context.fillRect(pt.x-w, pt.y-w, 2*w, 2*w, {fill:context.fillStyle})
+        // metrics
+        var text = node.data.name
+        context.font = '14pt Calibri';
+        context.textAlign = 'center';
+        var textMetrics = context.measureText(text);
+        var radius = (textMetrics.width) / 2 + 8 + node.data.radius;
+        
+        // drawing
+        context.beginPath();
+        context.arc(pt.x, pt.y, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = node.data.color;
+        context.fill();
+        context.lineWidth = 4;
+        context.strokeStyle = 'black';
+        context.stroke();
+        context.fillStyle = 'black';
+        context.fillText(text, pt.x, pt.y + 6)
       })    			
     },
   }
