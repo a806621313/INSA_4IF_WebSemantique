42f3d6cc1fb7adb27eca96722b74a67e5a018f37
AlexisLeConte <alexis.leconte@outlook.fr>
2018-11-13 17:54:32 +0100

    Updated ResourceServices interface

--- project/src/main/java/project/controller/services/ResourceServices.java
+++ project/src/main/java/project/controller/services/ResourceServices.java
@@ -1,35 +1,45 @@
 package project.controller.services;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ResourceServices {
-  private static Map<String, String> companies = new HashMap<String, String>();
-  private static Map<String, String> films = new HashMap<String, String>();
-  private static Map<String, String> persons = new HashMap<String, String>();
+  
+  /** A local database of company names and URIs (key = name ; value = URI) */
+  private final static Map<String, String> COMPANIES = new LinkedHashMap<String, String>();
+  /** A local database of film names and URIs (key = name ; value = URI) */
+  private final static Map<String, String> FILMS = new LinkedHashMap<String, String>();
+  /** A local database of person names and URIs (key = name ; value = URI) */
+  private final static Map<String, String> PERSONS = new LinkedHashMap<String, String>();
   
   /**
-   *
+   * Initializes the application local resource databases.
    */
   public static void init() {
-    // query all companies
-    
-    // query all films
-    
-    // query all persons
+    // Fill the local resource databases
     
   }
   
-  public static List<String> getCompaniesUriFromQuery(String query) {
+  public static Map<String, String> matchCompaniesByName(String name) {
+    return matchResourcesByName(name, COMPANIES);
+  }
+  
+  public static Map<String, String> matchFilmsByName(String name) {
+    return matchResourcesByName(name, FILMS);
+  }
+  
+  public static Map<String, String> matchPersonsByName(String name) {
+    return matchResourcesByName(name, PERSONS);
+  }
+  
+  private static Map<String, String> matchResourcesByName(String name, Map<String, String> res) {
+    // Find resource names that contain query words
+    
+    // If no resource name matches the query, compute Levenshtein distances
+    
+    // Sort the resources by relevance (number of matches or lowest Levenshtein distance)
+    
     return null;
   }
   
-  public static List<String> getFilmsUriFromQuery(String query) {
-    return null;
-  }
-  
-   public static List<String> getPersonsUriFromQuery(String query) {
-    return null;
-  }
 }
