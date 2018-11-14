0a802fe202380a4b557c1fc841e2bcef6666e5cd
Christophe GIBERT <christophe.gibert69@gmail.com>
2018-11-14 10:25:38 +0100

    Add queryByName method and changes on the html and css files

--- project/src/main/webapp/requests.js
+++ project/src/main/webapp/requests.js
@@ -1,12 +1,31 @@
 
 function loadSuggestions() {
-    
+
 }
 
 function queryByName() {
-    
+  var action = "queryByName";
+  var query = $('#research-bar').val();
+  
+  $("search-button").on("click", function(){
+    $.ajax({
+    url: './ActionServlet',
+    method: 'GET',
+    data: {
+      action: action,
+      query: query
+    },
+    dataType: 'json'
+  }).done(function (data) {
+    if (data.responseType === "queryResult") {
+      for (content in data.responseContent) {
+        $('#query-result').append("<li>" + content.ressourceName + " " + content.ressourceType + "</li>");
+      }
+    }
+  });
+  });
 }
 
 function queryByUri(uri) {
-    
+
 }
